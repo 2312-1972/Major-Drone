fb915580ace6a33c0fa30b5d47315d78a9b652b2
TOPPAN Frederic <toppan.frederic@gmail.com>
2024-02-21 15:00:33 +0100

    message for patience ok

--- major/src/App.js
+++ major/src/App.js
@@ -10,7 +10,9 @@ function App() {
         <h1 class="title h1border">MAJOR DRONE</h1>
         <h2 className="hmessage">Bientôt sur vos écrans ! </h2>
         <h3 className="hmessage">(On your screens soon !)</h3>
-
+        <p align="center">
+       <img className="giftdrone" align="center" width="100%"  alt="GIF" src="https://media.giphy.com/media/Q94TX0ecoNtTadxhvx/giphy.gif"/>
+       </p>
         </div>
         
       </header>

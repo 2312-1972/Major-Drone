882ad3583cd94a73818f2c53e6578e77e0eef0bb
TOPPAN Frederic <toppan.frederic@gmail.com>
2024-02-23 10:53:30 +0100

    nth-check maj

--- major/src/components/Header.js
+++ major/src/components/Header.js
@@ -9,10 +9,13 @@ function Header() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 class="title h1border">MAJOR DRONE</h1>
         <h2 className="hmessage">Bientôt sur vos écrans ! </h2>
-        <h3 className="hmessage">(On your screens soon !)</h3>
+        <div className="alignfundrone">
+            <h3 className="hmessage">(On your screens soon !)</h3>
+        <img className="giftdrone" align="center" width="10%"  alt="logo" src={funLogo} />
+        </div>
         <p align="center">
        <img className="giftdrone" align="center" width="100%"  alt="GIF" src="https://media.giphy.com/media/Q94TX0ecoNtTadxhvx/giphy.gif"/>
-       <img className="giftdrone" align="center" width="20%"  alt="logo" src={funLogo} />
+      
        </p>
         </div>
         

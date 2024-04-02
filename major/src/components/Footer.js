c8ba71314f95cedfc46472c8f83d61be05f07cc9
shenzor <toppan.frederic@gmail.com>
2024-04-02 16:49:54 +0200

    mise en place footer

--- major/src/components/Footer.js
+++ major/src/components/Footer.js
@@ -1,11 +1,34 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaYoutube, FaLinkedin, FaEnvelope, FaFacebook } from 'react-icons/fa';
 import '../App.css';
 
 
 function Footer() {
   return (
-    <div >
-     
-    </div>
+    <footer id='CV' className='footer'>
+      <div >
+        
+       <Link to={'https://www.youtube.com/@Major_Drone'} aria-label="Youtube" className='linkFooter' >
+          <span className='spanFooter'>Youtube</span>
+          <FaYoutube />
+        </Link>
+       <Link to={'https://www.linkedin.com/in/guillaume-marin-ab0875269/'} aria-label="LinkedIn" className='linkFooter'>
+          <span className='spanFooter'>LinkedIn</span>
+          <FaLinkedin />
+        </Link>
+        <Link to={'https://www.facebook.com/MajorDrone'} aria-label="Facebook" className='linkFooter'>
+          <span className='spanFooter'>Facebook</span>
+          <FaFacebook />
+        </Link>
+        <Link to={'./contact'} aria-label="Contact" className='linkFooter'>
+          <span className='spanFooter'>Contact</span>
+          <FaEnvelope />
+        </Link>
+      </div>
+   <h4 style={{ fontSize: '15px', color: 'black', width: 'auto',fontFamily:'oxygen',fontWeight:'500'}}><Link className='linkCopyright' to ={'https://portfolio-frederic-toppan.vercel.app/'}>©2023 The Art of Code </Link>, Tous droits réservés </h4>
+    </footer>
   );
 }
 

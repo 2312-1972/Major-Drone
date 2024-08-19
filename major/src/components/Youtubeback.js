e024f6e7cbb27ea2044ca4d852e41148d44f0934
Frédéric.Toppan <toppan.frederic@gmail.com>
2024-08-19 15:37:16 +0200

    Maj vidéo prez et photo profil

--- major/src/components/Youtubeback.js
+++ major/src/components/Youtubeback.js
@@ -1,18 +1,15 @@
-import React from 'react';
-
 const YouTubeBackground = () => {
   return (
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/y36FGk90dT4?autoplay=1&mute=1&loop=1&playlist=y36FGk90dT4"
+      src="https://www.youtube.com/embed/dc2REQTskM8?autoplay=1&mute=1&loop=1&playlist=dc2REQTskM8"
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
       allowFullScreen
     ></iframe>
-    
   );
 };
 
--- major/src/pages/Home.js
+++ major/src/pages/Home.js
@@ -1,10 +1,10 @@
 import '../App.css';
-
+import faceHome from '../faceHome.jpeg'
 
 function Homes() {
   return (
     <div >
-     
+     <img src={faceHome} className="logo1b " alt="profil"  />
   
     </div>
   );
